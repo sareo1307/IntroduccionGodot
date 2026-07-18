2307466890e9bce817c10815768544cba45435ff
Sebastian <125764987+sareo1307@users.noreply.github.com>
2026-07-18 11:02:41 -0500

    ExplosiveGD

--- Scenes/Explosivo.gd
+++ Scenes/Explosivo.gd
@@ -2,21 +2,21 @@ extends CharacterBody3D
 
 
 const SPEED = 0.5
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 1.5
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var sensivity = 0.003
-@onready var camera = $Camera3D
+var mouse_sensivity = 0.003
+@onready var camera3D =$Camera3D
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * sensivity)
-		camera.rotate_x(-event.relative.y * sensivity)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(70))
+		rotate_y(-event.relative.x * mouse_sensivity)
+		camera3D.rotate_x(-event.relative.y * mouse_sensivity)
+		camera3D.rotation.x = clamp(camera3D.rotation.x, deg_to_rad(-60), deg_to_rad(70))
 
 func _process(delta):
 	if Input.is_action_just_pressed("escape"):
